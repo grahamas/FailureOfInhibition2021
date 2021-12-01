719d897ad1569c05e3e8b01838487f7aa479fc3f
DrWatson <no@mail>
2021-11-30 23:44:00 -0700

    Make post-nicolas loading method standard

--- scripts/load/he_sweep_arrs.jl
+++ scripts/load/he_sweep_arrs.jl
@@ -1,69 +1,37 @@
-
-using DrWatson, StaticArrays, AxisIndices, NamedDims
-
-if !@isdefined(refresh_sweep_arrs)
-    refresh_sweep_arrs = false
-end
-
-if !@isdefined(A_sweep_lower_bound)
-    A_sweep_lower_bound = 0.1
-end
-
-if !@isdefined(A_sweep_upper_bound)
-    A_sweep_upper_bound = 1.5
-end
-
-if !@isdefined(A_sweep_dim_size)
-    A_sweep_dim_size = 10
-end
-
-
-@time if !@isdefined(blocking_fp_arr) || !@isdefined(monotonic_fp_arr) || refresh_sweep_arrs
-    
-    nullcline_mods = (
-        α=(1.0, 1.0), 
-        firing_θI=0.2, blocking_θI=0.5, 
-        n_lattice = 2,
-        save_idxs=nothing, save_on=false, saveat=0.1
-    ) 
-    file, filename = produce_or_load(datadir(), Dict("lower" => A_sweep_lower_bound, "upper"=>A_sweep_upper_bound, "length" => A_sweep_dim_size); 
-        prefix = "blocking_fp_arr",
+function get_fp_arr_data(nonl_name, lb, ub, len; 
+        technical_mods=(
+            n_lattice = 2, 
+            save_idxs=nothing, 
+            save_on=false, 
+            saveat=0.1
+        ),
+        mods=(;),
+        name_mapping=[],
+        refresh_sweep_arrs=false
+        )
+    file, filename = produce_or_load(datadir(), 
+        Dict("lb" => lb, 
+             "ub"=> ub, 
+             "len" => len, 
+             [name => val_fn(mods) for (name, val_fn) in pairs(name_mapping)]...); 
+        prefix = "$(nonl_name)_fp_arr",
         suffix = "bson",
-        force = refresh_sweep_arrs
+        force = refresh_sweep_arrs,
+        tag=false
     ) do c
-        A_range = range(c["lower"], c["upper"], length=A_sweep_dim_size)
+        A_range = range(c["lb"], c["ub"], length=c["len"])
         nullcline_sweeping_mods = (Aee=A_range, Aei=A_range, Aie=A_range, Aii=A_range)
-        blocking_nullcline_mods = nullcline_mods
-        blocking_prototype_name = "full_dynamics_blocking"
-        blocking_fp_arr = wcm_sweep_calculate_fixedpoints(
-            blocking_prototype_name, 
-            blocking_nullcline_mods,
+        nullcline_mods = merge(technical_mods, mods)
+        prototype_name = "full_dynamics_$(nonl_name)"
+        fp_arr = wcm_sweep_calculate_fixedpoints(
+            prototype_name, 
+            nullcline_mods,
             nullcline_sweeping_mods,
             ; 
             axis_length=100
         )
-        return @dict(blocking_fp_arr, blocking_nullcline_mods, blocking_prototype_name)
+        return @dict(fp_arr, nullcline_mods, prototype_name)
     end
-    @unpack blocking_fp_arr = file #, blocking_nullcline_mods, blocking_prototype_name = file
-    file, filename = produce_or_load(datadir(), Dict("lower" => A_sweep_lower_bound, "upper"=>A_sweep_upper_bound, "length" => A_sweep_dim_size); 
-        prefix = "monotonic_fp_arr",
-        suffix = "bson",
-        force = refresh_sweep_arrs
-    ) do c
-        A_range = range(c["lower"], c["upper"], length=A_sweep_dim_size)
-        nullcline_sweeping_mods = (Aee=A_range, Aei=A_range, Aie=A_range, Aii=A_range)
-        monotonic_nullcline_mods = nullcline_mods
-        monotonic_prototype_name = "full_dynamics_monotonic"
-        monotonic_fp_arr = wcm_sweep_calculate_fixedpoints(
-            monotonic_prototype_name, 
-            monotonic_nullcline_mods,
-            nullcline_sweeping_mods,
-            ; 
-            axis_length=100
-        )
-        return @dict(monotonic_fp_arr, monotonic_nullcline_mods, monotonic_prototype_name)
-    end
-    @unpack monotonic_fp_arr = file #, monotonic_nullcline_mods, monotonic_prototype_name = file
+    @unpack fp_arr, nullcline_mods, prototype_name = file
+    return (fp_arr, nullcline_mods, prototype_name)
 end
-
-refresh_sweep_arrs = false
